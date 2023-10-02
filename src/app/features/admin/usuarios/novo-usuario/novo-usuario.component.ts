fe48ff82314b720c07573afb599761d195a0a495
Mathues Piltz <matheus.piltz@ufms.br>
2023-10-01 20:22:31 -0400

    feat: exibindo a imagem do usuario quando a foto é carregada

--- src/app/features/admin/usuarios/novo-usuario/novo-usuario.component.ts
+++ src/app/features/admin/usuarios/novo-usuario/novo-usuario.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./novo-usuario.component.css']
 })
 export class NovoUsuarioComponent {
+  nomeArquivo! : string;
+  imagem : string = "../../../../../assets/icons/add-user.svg";
 
+  checkArquivo(event : any){
+    if(event.target.files[0].type == "image/jpeg"){
+      this.nomeArquivo =event.target.files[0].name;
+      this.imagem = URL.createObjectURL(event.target.files[0]);
+    }
+    else{
+      this.nomeArquivo = "Arquivo inválido, utilize um arquivo no formato .jpeg";
+      this.imagem = "../../../../../assets/icons/add-user.svg";
+      event.target.value = null;
+    }
+   }
 }
