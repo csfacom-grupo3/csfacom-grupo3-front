8470fe7ee60381034ba2f61dc45a50f06b87e070
Calel <thaisla_renner@hotmail.com>
2023-11-09 17:25:50 -0400

    telas de contatos finalizada

--- src/app/features/public/contatos/contatos.component.ts
+++ src/app/features/public/contatos/contatos.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 @Component({
   selector: 'app-contatos',
   templateUrl: './contatos.component.html',
   styleUrls: ['./contatos.component.css']
 })
-export class ContatosComponent {
+export class ContatosComponent implements OnInit{
+  title = 'google-maps';
 
+  ngOnInit(): void {
+    let loader =  new Loader({
+      apiKey: 'AIzaSyBd0NMxydNEw-hM-sD2y9ws2x7yZDUUZQw'
+    });
+
+    loader.load().then(()=> {
+      new google.maps.Map(document.getElementById("mapa")!, {
+        center: { lat: -20.5025212, lng: -54.6160359 },
+        zoom: 15
+      })
+    })
+  }
 }
