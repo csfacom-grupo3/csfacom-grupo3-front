3278d9756e01a9e241de585cf6c8e10f7ee70637
Mathues Piltz <matheus.piltz@ufms.br>
2023-11-28 21:47:19 -0400

    [feat] integração da lista de projetos publica

--- src/app/features/public/lista-projetos/lista-projetos.component.ts
+++ src/app/features/public/lista-projetos/lista-projetos.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/core/service/Api.Service';
 
 @Component({
   selector: 'app-lista-projetos',
   templateUrl: './lista-projetos.component.html',
   styleUrls: ['./lista-projetos.component.css']
 })
-export class ListaProjetosComponent {
+export class ListaProjetosComponent implements OnInit{
+  projetos! : any[];
+  
+  constructor(private _apiService : ApiService) {
+      
+  }
+
+  ngOnInit(): void {
+    this._apiService.get('projects')
+    .subscribe((data) => {
+      this.projetos = data;
+    });
+  }
 
 }
