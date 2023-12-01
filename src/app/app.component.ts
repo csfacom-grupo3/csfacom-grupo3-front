90520fc3795ca3f3a59929ccd14de3d84f5ad490
Mathues Piltz <matheus.piltz@ufms.br>
2023-11-30 23:55:55 -0400

    [feat]: deslogar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  usuarioLogado() : boolean{
+    return sessionStorage.getItem('email') ? true : false;
+  }
+
   title = 'portal-ledes';
 }
